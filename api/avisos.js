233a4a3c76393770de3947de8885c2013d8773ae
elMatidelUru <30472800+elMatidelUru@users.noreply.github.com>
2017-11-02 12:22:02 -0500

    Update avisos.js

--- api/avisos.js
+++ api/avisos.js
@@ -25,18 +25,97 @@ function Get_Avisos(callback){
 	}
 	callback(results,errors);
  }
+function Put_aviso(data,callback){
+	const testFolder = './avisos_json/';
+	const fs = require('fs');
+	var results = "OK";
+	var errors = "";
+	if (! data.file_name || ! data.file_content ) {
+		callback("","You did not specify a name for the file or there is no content");
+		return;
+	   }
+	try {
+		fs.writeFileSync(testFolder + "/" + data.file_name + ".json", data.file_content);
+	} catch (err) {
+		console.log(err);
+		callback(results,err);
+	}
+	callback(results,errors);
+ }
 // END Functions
 
+
+// GET Methdods
 app.get('/get_avisos', function (req, res) {
   Get_Avisos( function (results,errors){
 	  if(errors) {
-		res.status(500).end('Something weird happened!!');
+		res.header('Access-Control-Allow-Origin', '*');
+    		res.header('Access-Control-Allow-Methods', '*');
+    		res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+		res.header('Content-Type', 'text/html'); 
+		res.status(500).end('Something weird happened!! : ' + errors);
 	  } else {
+		res.header('Access-Control-Allow-Origin', '*');
+    		res.header('Access-Control-Allow-Methods', '*');
+    		res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+		res.header('Content-Type', 'application/json'); 
 		res.status(200).end(JSON.stringify(results));
 	  }
   });
   
 });
+
+// POST Methdods
+app.post('/put_aviso', function (req, res) {
+  if ( req.body.api_password == "shellbomb" ) {
+	  Put_aviso( req.body, function (results,errors){
+		  if(errors) {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Methods', '*');
+			res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+			res.header('Content-Type', 'text/html'); 
+			res.status(500).end('Something weird happened!! : ' + errors);
+		  } else {
+			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Methods', '*');
+			res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+			res.header('Content-Type', 'text/html'); 
+			res.status(200).end("Successfully uploaded file");
+		  }
+	  });
+  }else {
+  			res.header('Access-Control-Allow-Origin', '*');
+			res.header('Access-Control-Allow-Methods', '*');
+			res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+			res.header('Content-Type', 'text/html'); 
+			res.status(500).end('You are not authorized to do this!!');
+  }
+});
+
+
+//Enable CORS
+app.options('*', function (req, res) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).send("you cant just reply anything...thats where u are wrong kiddo!");
+  
+});
+
+// Safeguards
+app.get('*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(500).send('Unknown method!')
+});
+app.post('*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.status(500).send('Unknown method!')
+});
+
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT + '!');
