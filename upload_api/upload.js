a7b905494d35da9378710490293341fd42cc54fd
elMatidelUru <30472800+elMatidelUru@users.noreply.github.com>
2017-11-09 10:19:03 -0600

    Update upload.js

--- upload_api/upload.js
+++ upload_api/upload.js
@@ -18,7 +18,7 @@
         },
         filename: function (req, file, cb) {
             var datetimestamp = Date.now();
-            cb(null, "foto_avisos_" +  file.fieldname)
+            cb(null, "foto_avisos_" +  path.extname(file.originalname))
         }
     });
     var upload = multer({ //multer settings
