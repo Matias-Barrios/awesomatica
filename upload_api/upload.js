9525c3abbac158dc5b8d9f3c18ba2a3146823847
elMatidelUru <30472800+elMatidelUru@users.noreply.github.com>
2017-11-09 10:13:02 -0600

    Update upload.js

--- upload_api/upload.js
+++ upload_api/upload.js
@@ -23,13 +23,17 @@
     });
     var upload = multer({ //multer settings
                     storage: storage
-                }).single('file');
+                }).single('*');
     /** API path that will upload the files */
     app.post('/upload_aviso', function(req, res) {
         upload(req,res,function(err){
             if(err){
-                 res.json({error_code:1,err_desc:err});
-                 return;
+                res.header('Access-Control-Allow-Origin', '*');
+    	    	res.header('Access-Control-Allow-Methods', '*');
+    	    	res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+		        res.header('Content-Type', 'text/html'); 
+		        res.status(500).json({error_code:1,err_desc:err});
+                return;
             }
              res.json({error_code:0,err_desc:null});
         });
