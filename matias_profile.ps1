e1a09c6cf0e41684a438f4ccd31e1f09803d5436
elMatidelUru <30472800+elMatidelUru@users.noreply.github.com>
2017-11-20 14:28:45 -0300

    Add files via upload

---
+++ matias_profile.ps1
@@ -0,0 +1,17 @@
+﻿
+function Replace_Text ([String]$file_name, [String]$pattern,[String]$new) {
+
+
+    (Get-Content $file_name) -replace $pattern, $new | Set-Content  $file_name
+}
+
+function Get_random_file([String]$path ){
+
+    $all_items=$( Get-ChildItem $path '*.txt' );
+    return $all_items[$( Get-Random -minimum 0 -maximum $all_items.length )];
+}
+
+#Replace_Text "c:\test.txt" "bgmusic.*" "bgmusic = /sound/music.mp3"
+
+Get_random_file "c:\"
+
