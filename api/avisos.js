c9b88cb66c4581e430ef13e176d92619ae19162a
elMatidelUru <30472800+elMatidelUru@users.noreply.github.com>
2017-11-27 13:49:31 -0600

    Update avisos.js

--- api/avisos.js
+++ api/avisos.js
@@ -197,7 +197,7 @@ app.get('/get_horoscopo', function (req, res) {
   
 });
 app.get('/get_aviso', function (req, res) {
-  var query = url.parse(request.url, true).query;
+  var query = url.parse(req.url, true);
   Get_Aviso( query.aviso_id,function (results,errors){
 	  if(errors) {
 		res.header('Access-Control-Allow-Origin', '*');
